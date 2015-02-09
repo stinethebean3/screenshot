b0e5f1f932e627c4244e1cb595e866f729a1475b
Christine Vick <cdevlin23.cd@gmail.com>
2015-02-08 20:44:11 -0800

    refactored url

--- screenshot.sh
+++ screenshot.sh
@@ -2,11 +2,10 @@
 
 SSH_USERNAME="stinethebean"
 SSH_HOSTNAME="admin.hashbang.sh"
-PUBLIC_URL="stinethebean.hashbang.sh"
+PUBLIC_URL="http://stinethebean.hashbang.sh/$SCREENSHOT_FILE"
 PUBLIC_FOLDER="Public/"
 SCREENSHOT_DIR="$HOME/.screenshots"
 SCREENSHOT_FILE="$(date +%y-%m-%d-%H-%M-%S).png"
-FULL_URL="http://$PUBLIC_URL/$SCREENSHOT_FILE"
 
 mkdir -p "$SCREENSHOT_DIR"
 
@@ -14,6 +13,6 @@ import "$SCREENSHOT_DIR/$SCREENSHOT_FILE"
 
 scp "$SCREENSHOT_DIR/$SCREENSHOT_FILE" "$SSH_USERNAME@$SSH_HOSTNAME:$PUBLIC_FOLDER"
 
-echo "$FULL_URL"
+echo "$PUBLIC_URL"
 
-echo "$FULL_URL" | xclip
+echo "$PUBLIC_URL" | xclip
